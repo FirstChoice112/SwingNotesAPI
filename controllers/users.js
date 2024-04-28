db9df6d9aee29d51be3612e6d4102c259c625a91
FirstChoice112 <johan.skoog.1986@gmail.com>
2024-04-28 14:11:55 +0200

    lösenordet är nu hashat med bcryptjs

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,13 @@
 const UserModel = require("../models/usersModel");
+const bcrypt = require("bcryptjs");
 //Function to create a new user
 async function createUser(req, res, next) {
   try {
-    console.log(req.body);
     const { username, password } = req.body;
-    const newUser = { username, password };
-    console.log(newUser);
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = { username, password: hashedPassword };
     const savedUser = await UserModel.insert(newUser);
+
     res.status(201).json({ username: username, password: password });
   } catch (error) {
     next(error);
@@ -14,3 +15,7 @@ async function createUser(req, res, next) {
 }
 
 module.exports = { createUser };
+
+// I detta exempel används bcrypt.hash() för att hasha lösenordet med en säkerhetsnivå (salt) på 10.
+// Det hashade lösenordet sparas sedan i databasen istället för det råa lösenordet. När användaren loggar in senare kan du använda bcrypt.compare()
+// för att jämföra det hashade lösenordet med det som användaren anger vid inloggning.
