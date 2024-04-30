93da61b7064269b6ec9c33ec5b35fe03851b597b
FirstChoice112 <johan.skoog.1986@gmail.com>
2024-04-30 13:34:42 +0200

    har ändrat formateringen på tidsstämpeln och inmatningen av title och text i databasen

--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -2,7 +2,7 @@ const NoteModel = require("../models/notesModel");
 const uuid = require("uuid");
 const moment = require("moment");
 const notesController = require("../controllers/notes");
-const { makeNote } = notesController;
+const { makeNote } = require("../controllers/notes");
 //Get endpoint to get all notes
 const getAllNotes = async (req, res) => {
   try {
@@ -14,26 +14,28 @@ const getAllNotes = async (req, res) => {
   }
 };
 
+// Funktion för att formatera datum till "HH:mm"
+function formatTime(date) {
+  return moment(date).format("HH:mm");
+}
+
 //Post endpoint to create a new note
 const createNote = async (req, res) => {
   try {
-    const userId = req.user.userId;
     const { title, text } = req.body;
+    console.log(req.body);
     const id = uuid.v4();
-    const createdAt = moment().toISOString();
-    const modifiedAt = createdAt;
+    const createdAt = formatTime(new Date());
 
     const newNote = await makeNote({
       id,
       title,
       text,
       createdAt,
-      modifiedAt,
-      userId,
     });
-    res.status(201).json(newNote);
+    res.status(201).json({ success: true, note: newNote });
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(400).json({ success: false, message: error.message });
   }
 };
 
@@ -41,6 +43,7 @@ const createNote = async (req, res) => {
 const updateNote = async (req, res) => {
   try {
     const userId = req.user.userId;
+    // const modifiedAt = formatTime(new Date());
     const updatedNote = await NoteModel.updateNote(
       userId,
       req.params.id,
